8beb7d53c14b27dd85349c3d197892fd2fbb334e
Kent Inverarity <kinverarity@hotmail.com>
2018-10-07 19:58:45 +1030

    "update jupyter notebooks" (via pypkg docs)

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# List of changes
+
+## Version 0.0
---
+++ setup.py
@@ -0,0 +1,28 @@
+'''Setup script for ozburn'''
+
+from setuptools import setup
+
+setup(
+    name='ozburn',
+    version='0.0',
+    description='ARPANSA UV index monitoring vs modelled data ',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kinverarity1/ozburn',
+    author='Kent Inverarity',
+    author_email='kinverarity@hotmail.com',
+    license='MIT',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        ),
+    keywords='python',
+    packages=['ozburn', ],
+    install_requires=['requests'],
+)
